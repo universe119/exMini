abe60d6c19b24efd4b0d42ad6486e35085320e70
universe119 <ruddh119@gmail.com>
2024-09-20 17:10:56 +0900

    버튼 활성화 기능 추가

--- js/script.js
+++ js/script.js
@@ -30,11 +30,21 @@ btns.forEach((btn, idx) => {
 		console.log(idx);
 
 		new Anime(panel, { left: -100 * idx + "%" }, { duration: 500 });
-
 		// panel.style.marginLeft = -400 * idx + "px";
+
+		// 버튼 클릭시마다 순간적으로 모든 버튼을 반복돌며 on을 제거
+		btns.forEach((btn) => btn.classList.remove("on"));
+		// idx에 해당하는 현재 순번의 버튼만 on을 붙여 활성화
+		btns[idx].classList.add("on");
 	});
 });
 
 // 미션1 - 위의 코드에서 각각의 버튼을 클릭하면 해당 순번의 패널이 프레임 안쪽으로 들어오도록 new Anime를 활용해 좌우 슬라이더 모션 처리 (3시 50분까지)
 
 // 미션2 - HTML에서 li의 갯수만 수정하면 scss에서 일일이 panel, li의 넓이를 수정할 필요가 없도록 스크립트에서 동적 처리.(4시 40분까지)
+
+/*
+특정 요소만 활성화 시키는 로직
+	- 순간적으로 모든 그룹요소를 비활성화 처리
+	- 그 직후 원하는 요소만 활성화 처리
+*/
